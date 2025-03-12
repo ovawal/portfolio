96a22c3669adec84e7ecc5ef1bf3b1be9559ac80
Walter <wangakiokova@gmail.com>
2025-03-12 14:12:23 -0500

    avatr to avator

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -7,7 +7,7 @@ const person = {
     return `${this.firstName} ${this.lastName}`;
   },
   role: "Linux Sytem Engineer",
-  avatr: "/images/avator.jpg",
+  avator: "/images/avator.jpg",
   location: "America/Chicago", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English"], // optional: Leave the array empty if you don't want to display languages
 };
